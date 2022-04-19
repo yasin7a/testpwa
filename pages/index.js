268c9e5381d5b4756f3ddbcfe01d25fedc5c0e9f
Yasin <arafath7yasin@gmal.com>
2022-04-18 23:10:29 -0700

    gh

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Link from "next/link";
 import Head from "next/head";
+import Cookies from "js-cookie";
 export default function Home() {
   const [installable, setInstallable] = React.useState(false);
   const [deferredPrompt, setDeferredPrompt] = React.useState(null);
+  const [delPop, setDelpop] = React.useState(false);
+  const [skip, setSkip] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
   React.useEffect(() => {
     window.addEventListener("beforeinstallprompt", (e) => {
       e.preventDefault();
@@ -12,11 +16,18 @@ export default function Home() {
     });
 
     window.addEventListener("appinstalled", () => {
+      setInstallable(false);
       console.log("INSTALL: Success");
     });
+
+    Cookies.get("skipPWA") && setSkip(true);
   }, []);
+  React.useEffect(() => {
+    setLoading(false);
+    Cookies.get("skipPWA") && setSkip(true);
+  }, []);
+
   const handleInstallClick = (e) => {
-    setInstallable(false);
     deferredPrompt.prompt();
     deferredPrompt.userChoice.then((choiceResult) => {
       if (choiceResult.outcome === "accepted") {
@@ -25,6 +36,15 @@ export default function Home() {
         console.log("User dismissed the install prompt");
       }
     });
+  };
+
+  const handleDelete = () => {
+    setDelpop(true);
+  };
+  const handleSkip = () => {
+    setDelpop(true);
+    Cookies.set("skipPWA", "true", { expires: 2, path: "/" });
+    alert("pwa pop up deleted for 2 days");
   };
   console.log("render");
   return (
@@ -37,11 +57,19 @@ export default function Home() {
         <a> About </a>
       </Link>
       <h2>Home page !!!</h2>
-      {installable && (
-        <button className="install-button" onClick={handleInstallClick}>
-          INSTALL ME
-        </button>
-      )}
+      {!loading &&
+        (skip ||
+          (installable && (
+            <div
+              className="pwa-pop"
+              style={{ display: delPop ? "none" : "block" }}
+            >
+              <h1> install app for better experience</h1>
+              <button onClick={handleInstallClick}>INSTALL</button>
+              <button onClick={handleDelete}>DELETE</button>
+              <button onClick={handleSkip}>SKIP</button>
+            </div>
+          )))}
     </>
   );
 }

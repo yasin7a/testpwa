078a6e9a64362fb4ac3cf4a974f57b2e47510add
Yasin <arafath7yasin@gmal.com>
2022-04-18 19:22:17 -0700

    sdfsd

--- pages/index.js
+++ pages/index.js
@@ -1,86 +1,35 @@
 import React from "react";
 import Link from "next/link";
 import Head from "next/head";
-
+let deferredPrompt;
 export default function Home() {
-  let addBtn = React.useRef();
-  let [popout, setpopout] = React.useState(false);
-  let [ispop, setIspop] = React.useState(false);
-  let handlePop = () => {
-    setpopout(true);
-  };
+  const [installable, setInstallable] = React.useState(false);
+  React.useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      e.preventDefault();
+      deferredPrompt = e;
+      setInstallable(true);
+    });
 
-  React.useLayoutEffect(() => {
-    window.onload = () => {
-      let deferredPrompt;
-
-      // if (
-      //   window.matchMedia("(display-mode: standalone)").matches ||
-      //   window.navigator.standalone === true
-      // ) {
-      //   setIspop(true);
-      //   setpopout(true);
-
-      //   deferredPrompt = null;
-      // }
-
-      window.addEventListener("beforeinstallprompt", (e) => {
-        e.preventDefault();
-        deferredPrompt = e;
-        listenToUserAction();
-      });
-      function listenToUserAction() {
-        addBtn.current?.addEventListener("click", actions);
-      }
-
-      let actions = () => {
-        setpopout(true);
-
-        deferredPrompt?.prompt();
-        deferredPrompt?.userChoice.then((choice) => {
-          if (choice.outcome === "accepted") {
-            console.log("User accepted");
-            setIspop(true);
-          } else {
-            console.log("User dismissed");
-          }
-        });
-        deferredPrompt = null;
-      };
-      window.addEventListener("appinstalled", () => {
-        deferredPrompt = null;
-        setIspop(true);
-        console.log("PWA was installed");
-      });
-    };
+    window.addEventListener("appinstalled", () => {
+      console.log("INSTALL: Success");
+    });
   }, []);
-
+  const handleInstallClick = (e) => {
+    setInstallable(false);
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the install prompt");
+      } else {
+        console.log("User dismissed the install prompt");
+      }
+    });
+  };
+  console.log("render");
   return (
     <>
-
-      {popout ? (
-        <></>
-      ) : (
-        <div style={{ display: ispop ? "none" : "block" }}>
-          <div
-            style={{
-              backgroundColor: "blue",
-              textAlign: "center",
-              position: "absolute",
-              left: 0,
-              right: 0,
-              bottom: 0,
-            }}
-          >
-            <h2>For better experience install in mobile</h2>
-            <button ref={addBtn}>Install</button>
-            <button onClick={handlePop} style={{ marginLeft: "200px" }}>
-              ❌
-            </button>
-          </div>
-        </div>
-      )}
-
       <Head>
         <title>Home page</title>
       </Head>
@@ -89,6 +38,11 @@ export default function Home() {
         <a> About </a>
       </Link>
       <h2>Home page</h2>
+      {installable && (
+        <button className="install-button" onClick={handleInstallClick}>
+          INSTALL ME
+        </button>
+      )}
     </>
   );
 }

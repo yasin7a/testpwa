ebf82b7c9e49b0b1a8a022c7fb7e77db94d62931
Yasin <arafath7yasin@gmal.com>
2022-04-22 05:22:34 -0700

    asedfasd

--- components/onSignal.jsx
+++ components/onSignal.jsx
@@ -3,56 +3,49 @@
 import React from "react";
 //************ for _document page ************************************/
 
-// class ReactOneSignal extends React.Component {
-//   oneSignal = () => {
-//     return {
-//       __html: `window.OneSignal = window.OneSignal || [];
-//       OneSignal.push(function() { OneSignal.init({ appId: "8e8bea0f-e465-437c-824e-968451c7537c" }); });`,
-//     };
-//   };
-
-//   render = () => {
-//     return (
-//       <>
-//         <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async />
-//         <script dangerouslySetInnerHTML={this.oneSignal()} />
-//       </>
-//     );
-//   };
-// }
-
-// export default ReactOneSignal;
-
-
-//************ for _app page ************************************/
-
-
-
-
-
-
-
-
-
-import Script from "next/script";
 class ReactOneSignal extends React.Component {
+  oneSignal = () => {
+    return {
+      __html: `window.OneSignal = window.OneSignal || [];
+      OneSignal.push(function() { OneSignal.init({ appId: "8e8bea0f-e465-437c-824e-968451c7537c" }); });`,
+    };
+  };
+
   render = () => {
     return (
       <>
-        <Script
-          src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"
-          strategy="beforeInteractive"
-        />
-        <Script
-          id="my-script"
-          dangerouslySetInnerHTML={{
-            __html: `window.OneSignal = window.OneSignal || []; 
-      OneSignal.push(function() { OneSignal.init({ appId: "8e8bea0f-e465-437c-824e-968451c7537c" }); });`,
-          }}
-        />
+        <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async />
+        <script dangerouslySetInnerHTML={this.oneSignal()} />
       </>
     );
   };
 }
 
 export default ReactOneSignal;
+
+
+//************ for _app page ************************************/
+
+
+// import Script from "next/script";
+// class ReactOneSignal extends React.Component {
+//   render = () => {
+//     return (
+//       <>
+//         <Script
+//           src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"
+//           strategy="beforeInteractive"
+//         />
+//         <Script
+//           id="my-script"
+//           dangerouslySetInnerHTML={{
+//             __html: `window.OneSignal = window.OneSignal || []; 
+//       OneSignal.push(function() { OneSignal.init({ appId: "8e8bea0f-e465-437c-824e-968451c7537c" }); });`,
+//           }}
+//         />
+//       </>
+//     );
+//   };
+// }
+
+// export default ReactOneSignal;
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import "../styles/globals.css";
-import ReactOneSignal from "../components/onSignal";
+// import ReactOneSignal from "../components/onSignal";
 
 import React from "react";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <ReactOneSignal />
+      {/* <ReactOneSignal /> */}
 
       <Component {...pageProps} />
     </>
